1f92959db518d581c7721cf17b2b5560a3d94013
Scott Chamberlain <myrmecocystus@gmail.com>
2016-03-31 14:34:39 -0700

    fix rproj file name, rbuildginore travis

--- R/find_deps.R
+++ R/find_deps.R
@@ -1,16 +1,23 @@
 #' Get all functions needed for a given function
 #'
 #' @export
+#' @param x an object of class function_map
 #' @param fxns One or more functions. required.
 #' @return A list
 #' @examples \dontrun{
-#' find_deps(...)
+#' res <- scattalyze("../rredlist")
+#' find_deps(res, fxns = "rl_citation")
 #' }
-find_deps <- function(fxns) {
+find_deps <- function(x, fxns) {
   UseMethod("find_deps")
 }
 
 #' @export
-find_deps.function_map <- function(fxns) {
-  "xxx"
+find_deps.default <- function(x, fxns) {
+  stop("find_deps doesn't have a method for ", class(x), call. = FALSE)
+}
+
+#' @export
+find_deps.function_map <- function(x, fxns) {
+  x$data[fxns]
 }
